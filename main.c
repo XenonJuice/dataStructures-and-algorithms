5229dd0b44233d342bea603ff485a030f65aff4a
Shinozaki <shinozaki9852@gmail.com>
2024-09-09 20:26:49 +0900

    add some test fucntion in main.c to test ArrayList.c and Heap.c

--- main.c
+++ main.c
@@ -5,19 +5,49 @@
 #include <stdlib.h>
 
 #include "AVL_TREE.h"
+#include "Queue.h"
+#include "Heap.h"
+#include "STACK.h"
+#include "ArrayList.h"
 
 
 int main() {
     system("chcp 65001");
     printf("Hello, Excel!\n");
     // test the avl tree
-    AVL_TreeNode *root = NULL;
-    for (int i = 0; i < 20; i++) {
-        root = insert(root, i);
+    // AVL_TreeNode *root = NULL;
+    // for (int i = 0; i < 20; i++) {
+    //     root = insert(root, i);
+    // }
+    // bfs(root);
+    // printTree(root, 0);
+    // destroyAVLTree(root);
+
+    // test the arraylist
+    // ArrayList *list = initArrayList(10);
+    // for (int i = 20; i > 0; i--) {
+    //     add(list, i);
+    // }
+
+    //test the heap
+    Heap *heap = heap_create(10);
+    for (int i = 20; i > 0; i--) {
+        heap_insert(heap, i);
     }
-    bfs(root);
-    printTree(root, 0);
-    destroyAVLTree(root);
+    heap_print(heap);
+    heap_print(heap);
+
+    heap_insert(heap, 0);
+    heap_print(heap);
+    heap_insert(heap, 0);
+    heap_insert(heap, 99);
+    heap_insert(heap, 50);
+    heap_insert(heap, 100);
+    heap_insert(heap, 21);
+    heap_insert(heap, -5);
+    heap_print(heap);
+    heap_destroy(heap);
+
 
     getchar();
     return 0;
