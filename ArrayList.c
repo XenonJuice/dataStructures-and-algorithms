46503bcac07d7d94d1ac6884adf00e36e2c52de4
Shinozaki <shinozaki9852@gmail.com>
2024-09-18 11:21:47 +0900

    fix insertionSort

--- ArrayList.c
+++ ArrayList.c
@@ -237,12 +237,13 @@ void insertionSort(ArrayList *list) {
     int *arr = list->data;
     int n = list->index;
     for (int i = 1; i < n; i++) {
-        int j= i-1;
-        int tmp =arr[i];
+        int j = i - 1;
+        int tmp = arr[i];
         while (j >= 0 && arr[j] > tmp) {
             arr[j + 1] = arr[j];
             j--;
         }
+        arr[j + 1] = tmp;
     }
 }
 
