3b631f976c0c4f7945f0c06e161b76adecd0cb13
xenonJuice <myEmail>
2024-09-20 14:31:54 +0900

    Implemented a doubly linked list and added some common functions.

---
+++ LinkedList.h
@@ -0,0 +1,40 @@
+//
+// Created by user on 2024/9/20.
+//
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+#include "ArrayList.h"
+
+typedef struct LinkedListNode linkedListNode;
+
+typedef struct LinkedListNode {
+    int val;
+    int specificID;
+    struct LinkedListNode *prev;
+    struct LinkedListNode *next;
+} LinkedListNode;
+
+// 创建链表
+LinkedListNode *createLinkedList(int val);
+
+// 插入节点
+LinkedListNode *insertLinkedList(LinkedListNode *head, int val);
+
+// 删除节点
+LinkedListNode *deleteLinkedList(LinkedListNode *head, int val);
+
+// 查找节点
+ArrayList *searchLinkedList(LinkedListNode *head, int val);
+
+// 打印链表
+void printLinkedList(LinkedListNode *head);
+
+// 获取链表长度
+void getLinkedListLength(LinkedListNode *head);
+
+// 释放链表
+void freeLinkedList(LinkedListNode *head);
+
+
+#endif //LINKEDLIST_H
